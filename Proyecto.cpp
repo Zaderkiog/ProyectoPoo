dc5d4c12192a366bd19422734eb9bbcbccee7ef6
Cristian Oviedo <101075590+Zaderkiog@users.noreply.github.com>
2025-05-22 12:00:04 -0500

    first

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,1 +1,39 @@
-#include <iosteam>;
+#include <iostream>
+
+// Esto es para acumula puntos, no sé que nombre ponerle
+class Sistema {
+
+};
+
+class Fruta {
+
+};
+
+class Carne {
+
+};
+
+class Pollo: public Carne{
+
+};
+
+class Cerdo: public Carne{
+
+};
+
+class Pescado: public Carne{
+
+};
+
+
+class Verduras {
+
+};
+
+class ArtIndividuales {
+
+};
+
+int main(){
+
+};
